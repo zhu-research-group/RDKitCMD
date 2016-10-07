2f18d13451142990c98aac7f0b1ea868d85080f6
Daniel P. Russo <russodanielp@gmail.com>
2016-10-07 17:10:02 -0400

    added descriptor calculator

--- caculate_descriptor.py
+++ caculate_descriptor.py
@@ -1,49 +1,24 @@
-from rdkit import Chem
+from rdkit.ML.Descriptors import MoleculeDescriptors
 from rdkit.Chem import Descriptors
+from rdkit import Chem
+import pandas as pd
+
+def calc_descriptors_from_file(filename):
+    """ calculates rdkit descriptors from a smiles.txt file """
+    df = pd.read_table(filename, header=None)
+    df['rdkit'] = [Chem.MolFromSmiles(smi) for smi in df[0]]
+    df[pd.isnull(df['rdkit'])].to_csv('errors.txt')
+    df = df[~pd.isnull(df['rdkit'])]
+    calc = MoleculeDescriptors.MolecularDescriptorCalculator([desc[0] for desc in Descriptors.descList])
+
+    X = pd.DataFrame([list(calc.CalcDescriptors(mol)) for mol in df['rdkit']],
+                     columns=list(calc.GetDescriptorNames()),
+                     index=df[0].values)
+
+    X.to_csv('descriptors.csv')
 
 
-smile_list = ['c1ccccc1C(=O)O','cc']
-
-
-def descrptor_1smiles(smiles_str):
-    m = Chem.MolFromSmiles(smiles_str)
-    descriptor = []
-    descriptor.append(Descriptors.HeavyAtomMolWt(m))
-    descriptor.append(Descriptors.BalabanJ(m))
-    descriptor.append(Descriptors.BertzCT(m))
-    descriptor.append(Descriptors.Ipc(m))
-    descriptor.append(Descriptors.HallKierAlpha(m))
-    descriptor.append(Descriptors.Kappa1(m))
-    descriptor.append(Descriptors.Kappa3(m))
-    descriptor.append(Descriptors.Chi0(m))
-    descriptor.append(Descriptors.Chi1(m))
-    descriptor.append(Descriptors.Chi0n(m))
-    descriptor.append(Descriptors.Chi4n(m))
-    descriptor.append(Descriptors.Chi0v(m))
-    descriptor.append(Descriptors.Chi4v(m))
-    descriptor.append(Descriptors.MolLogP(m))
-    descriptor.append(Descriptors.MolMR(m))
-    descriptor.append(Descriptors.MolWt(m))
-    descriptor.append(Descriptors.ExactMolWt(m))
-    descriptor.append(Descriptors.HeavyAtomCount(m))
-    descriptor.append(Descriptors.HeavyAtomMolWt(m))
-    descriptor.append(Descriptors.NHOHCount(m))
-    descriptor.append(Descriptors.NOCount(m))
-    descriptor.append(Descriptors.NumHAcceptors(m))
-    descriptor.append(Descriptors.NumHDonors(m))
-    descriptor.append(Descriptors.NumHeteroatoms(m))
-    descriptor.append(Descriptors.NumRotatableBonds(m))
-    descriptor.append(Descriptors.NumValenceElectrons(m))
-    descriptor.append(Descriptors.RingCount(m))
-    descriptor.append(Descriptors.FractionCSP3(m))
-    descriptor.append(Descriptors.NumAromaticHeterocycles(m))
-    descriptor.append(Descriptors.NumSaturatedHeterocycles(m))
-    descriptor.append(Descriptors.NumAliphaticHeterocycles(m))
-    descriptor.append(Descriptors.NumAromaticCarbocycles(m))
-    descriptor.append(Descriptors.NumSaturatedCarbocycles(m))
-    descriptor.append(Descriptors.NumAliphaticCarbocycles(m))
-    print(descriptor)
-    return descriptor
-
-
-descrptor_caculator('c1ccccc1C(=O)O')
+if __name__ == "__main__":
+    import sys
+    filename = sys.argv[1]
+    calc_descriptors_from_file(filename)
